824f4ca55177d5bc3fcb9576bc8747c7f86a9c4b
pratech143 <132917597+pratech143@users.noreply.github.com>
2024-05-12 15:13:06 +0000

    folder testing

---
+++ 01_Javascript/Arrays.js
@@ -0,0 +1,8 @@
+const Array1=['Pratik','he','him'];
+const Array2=[1,2,3,4,5];
+//concat of two arrays
+const Array3=Array1.concat(Array2);
+console.log(Array3);
+//using spread operator
+const Array4=[...Array1,...Array2]
+console.log(Array4);
